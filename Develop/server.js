1b61785b0c54c80f29f2878e6c0a9668943b7a13
Maridon Romney <70175497+mkromney@users.noreply.github.com>
2023-07-06 22:34:19 -0600

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,44 +3,49 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+// const dataJson = require('db.json');
 
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
-app.use('/api', api);
+// app.use('/api', api);
 
 
 // Routes to the notes.html file. //
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
-);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 });
+
+// For some reason the code doesn't work when these lines are not commented out. //
+
+// app.get('*', (req, res) => {
+//   // res.sendFile(path.join(__dirname, './public/index.html'));
+//   res.send('wrong route');
+// });
+
 
 
 // GET API routes that read the db.json file. //
 app.get('/api/notes', (req, res) => {
-  fs.readFile('db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Could not read notes.' });
     }
-
     const notes = JSON.parse(data);
-    res.json(notes);
+    return res.json(notes);
   });
 });
 
 
 // POST API routes that read the db.json file. //
 app.post('/api/notes', (req, res) => {
-  fs.readFile('db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Could not read notes.' });
@@ -56,7 +61,7 @@ app.post('/api/notes', (req, res) => {
 
     notes.push(newNote);
 
-    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Failed to save the new note.' });
@@ -68,7 +73,7 @@ app.post('/api/notes', (req, res) => {
 });
 
 // Event listener route at event path. //
-app.post('/api/notes/html/body/nav/div/i[2]', (req, res) => {
+app.post('/api/notes', (req, res) => {
   const eventData = req.body.eventData;
   // Handle the event data here
 
